e01874b262ba8625e0fa99ce40ad6425eb1b7e30
tom <13849343+tomFfff@user.noreply.gitee.com>
2024-06-09 20:54:29 +0800

    changes

--- src/components/common/floatButtonGroup.vue
+++ src/components/common/floatButtonGroup.vue
@@ -1,67 +1,28 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-
-const props = defineProps({
-  width: {
-    type: Number,
-    default: 50
-  },
-  height: {
-    type: Number,
-    default: 150
-  },
-  position :{
-    type: String,
-    default: 'fixed'
-  },
-  top: {
-    type: Number,
-    default: null
-  },
-  bottom: {
-    type: Number,
-    default: undefined
-  },
-  left: {
-    type: Number,
-    default:undefined
-  },
-  right: {
-    type: Number,
-    default: undefined
-  }
-})
-const Style = computed(() => {
-  return {
-    position: props.position,
-    top: props.top? `${props.top}px`:props.top,
-    right: props.right? `${props.right}px`:props.right,
-    bottom: props.bottom? `${props.bottom}px`:props.bottom,
-    left: props.left? `${props.left}px`:props.left,
-  }
-})
-
 </script>
 
 <template>
-  <div class="float-button-group" :style="Style">
+  <div class="float-button-group">
     <slot></slot>
   </div>
 </template>
 
 <style scoped lang="scss">
 .float-button-group {
+  box-sizing: content-box;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap:10px;
-  padding: 20px;
-  box-shadow:0 2px 8px 0px rgba(0, 0, 0, .16);
+  width: 100px;
+  border-radius: 15px;
+  gap: 10px;
+  padding: 25px;
+  box-shadow: 0 2px 8px 0px rgba(0, 0, 0, 0.16);
   background-color: #fff;
-  z-index:10;
-  transition: all .5s;
+  z-index: 10;
+  transition: all 0.5s;
   &:hover {
-    box-shadow: 0 2px 12px 0px rgba(0, 0, 0, .24);
+    box-shadow: 0 2px 12px 0px rgba(0, 0, 0, 0.24);
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import router from './router' // 导入 router
 import './styles/common.scss'
 import store from './store'
 
+// 如果您正在使用CDN引入，请删除下面一行。
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(store)
 app.use(router) // 使用 router
