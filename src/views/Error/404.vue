51b01dad698318994b41a09bb89e6802d30f1648
tom <13849343+tomFfff@user.noreply.gitee.com>
2024-07-13 15:04:28 +0800

    feat: 404页面添加返回首页功能

--- src/views/Error/404.vue
+++ src/views/Error/404.vue
@@ -1,15 +1,21 @@
 <script lang="ts" setup>
+import router from '@/router/index.ts'
 defineOptions({
-    group_name: '404-page'
+  group_name: '404-page',
 })
+
+const backHome = () => {
+  router.push({ path: '/' })
+}
 </script>
 
 <template>
-	<div>
+  <div>
     <h1>404 NOT FOUND!</h1>
+    <el-button type="primary" size="large" @click="backHome"
+      >回到首页</el-button
+    >
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
