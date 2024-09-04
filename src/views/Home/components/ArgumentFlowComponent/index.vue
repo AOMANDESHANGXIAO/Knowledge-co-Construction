b666da72e11fe461ce8eb307c93619b03ee7a684
tom <13849343+tomFfff@user.noreply.gitee.com>
2024-09-04 14:39:14 +0800

    refactor: 抽离反馈逻辑

--- src/views/Home/components/ArgumentFlowComponent/index.vue
+++ src/views/Home/components/ArgumentFlowComponent/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 // TODO: 重构组件
-import { h, ref } from 'vue'
+import { h, ref } from 'vue';
 import { VueFlow, Panel, useVueFlow } from '@vue-flow/core'
 import { Background } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
 import { useLayout } from '@/hooks/VueFlow/useLayout'
-import type { NodeType, EdgeType, AddBackPayload, FeedBack } from './type.ts'
+import type { NodeType, EdgeType, AddBackPayload } from './type.ts'
 import { Status } from './type'
 import { ArgumentType } from './type.ts'
 import { LayoutDirection } from './type.ts'
@@ -21,6 +21,7 @@ import { queryNodeContentApi } from '@/apis/flow'
 import { QueryNodeContentData } from '@/apis/flow/type'
 import { convertToHTML } from './utils'
 import { onMounted } from 'vue'
+import useFeedback from './hooks/useFeedback'
 
 const {
   createNode,
@@ -34,7 +35,6 @@ const {
   getNodeIdsByType,
   findIsEdgesExistBySourceId,
   clearNotRealatedEdges,
-  findIsEdgeExistByFilterFunction,
 } = useNodeEdgeHandler()
 
 const props = withDefaults(
@@ -133,7 +133,7 @@ onPaneReady(() => {
 })
 
 const handleLayoutGraph = async () => {
-  setNodesValue(layout(nodes.value, edges.value, 'LR'))
+  setNodesValue(layout(nodes.value, edges.value, 'TB'))
 
   await nextTick(() => {
     fitView()
@@ -163,120 +163,19 @@ onMounted(async () => {
 /**
  * 定义反馈逻辑
  */
-const feedbackList = ref<FeedBack[]>([])
-
-interface FBOpt {
-  [key: string]: {
-    title: string
-    description: string
-    type: 'success' | 'warning' | 'error' | 'info'
-  }
-}
-
-const feedbackOptions: FBOpt = {
-  warrant: {
-    title: '缺少辩护',
-    description: '需要一个辩护来解释前提和结论的关系',
-    type: 'warning',
-  },
-  qualifier: {
-    title: '缺少限定词',
-    description: '需要一个限定词来限制当前论点的范围',
-    type: 'warning',
-  },
-  rebuttal: {
-    title: '缺少反驳',
-    description: '需要一个反驳来委婉化当前论点',
-    type: 'warning',
-  },
-  backing: {
-    title: '缺少支撑',
-    description: '有一个辩护没有支撑!',
-    type: 'warning',
-  },
-  perfect: {
-    title: '论点完美',
-    description: '这是一个结构完整的论证!',
-    type: 'success',
-  },
-}
+const { feedbackList, feedbackCallback: feedbackFunction } = useFeedback()
 
 const feedbackCallback = () => {
-  const flagMap = {
-    warrant: false, // 辩护
-    rebuttal: false, // 反驳
-    qualifier: false, // 限定词
-  }
+  
+  const nodesValue = nodes.value
+  const edgesValue = edges.value
 
-  const warrantsList: NodeType[] = []
-
-  nodes?.value?.forEach(item => {
-    const _type = item.data._type
-
-    switch (_type) {
-      case ArgumentType.Warrant:
-        flagMap.warrant = true
-        warrantsList.push(item)
-        break
-      case ArgumentType.Qualifier:
-        flagMap.qualifier = true
-        break
-      case ArgumentType.Rebuttal:
-        flagMap.rebuttal = true
-        break
-      default:
-        break
-    }
-  })
-
-  // 查找有没有支撑
-  let hasBacking = false
-
-  for (let j = 0; j < warrantsList.length; j++) {
-    const item = warrantsList[j]
-    hasBacking = findIsEdgeExistByFilterFunction(edges.value, edge => {
-      return (
-        edge._type === `${ArgumentType.Backing}_${ArgumentType.Warrant}` &&
-        edge.target === item.id
-      )
-    })
-    // 没有支撑
-    if (!hasBacking) break
-  }
-
-  // 根据flagMap，判断是否生成反馈
-  let feedbacks = []
-  Object.keys(flagMap).forEach(key => {
-    if (!flagMap[key as keyof typeof flagMap]) {
-      feedbacks.push({
-        title: feedbackOptions[key as keyof typeof flagMap].title,
-        description: feedbackOptions[key as keyof typeof flagMap].description,
-        type: feedbackOptions[key as keyof typeof flagMap].type,
-      })
-    }
-  })
-
-  // 没有辩护时不会出现提示需要支撑
-  if (warrantsList.length && !hasBacking) {
-    feedbacks.push({
-      title: feedbackOptions.backing.title,
-      description: feedbackOptions.backing.description,
-      type: feedbackOptions.backing.type,
-    })
-  }
-
-  if (feedbacks.length === 0) {
-    feedbacks.push({
-      title: feedbackOptions.perfect.title,
-      description: feedbackOptions.perfect.description,
-      type: feedbackOptions.perfect.type,
-    })
-  }
-
-  feedbackList.value = [...feedbacks]
+  feedbackFunction(nodesValue, edgesValue)
 }
 
-feedbackCallback()
+onMounted(() => {
+  feedbackCallback()
+})
 
 // =========================================
 
